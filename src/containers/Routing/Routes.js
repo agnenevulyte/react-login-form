78a45c119650f4a879263ee5c01c4519ef9aa45a
agnenevulyte <you@example.com>
2019-12-30 16:17:38 +0000

    add the route for the Login component

--- src/containers/Routing/Routes.js
+++ src/containers/Routing/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../Home';
-import NotFound from './NotFound'
+import Login from '../Login';
+import NotFound from './NotFound';
 
 const Routes = () => (
 	<Switch>
 		<Route exact path="/" component={Home} />
+		<Route path='/login' exact component={Login}/>
 		<Route component={NotFound} />
 	</Switch>
 );
