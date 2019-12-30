711fe09bc6010aa87bf44907910c13ee34994ac9
agnenevulyte <you@example.com>
2019-12-30 17:31:19 +0000

    Routes.js, from Route to AppliedRoute where needed

--- src/containers/Routing/Routes.js
+++ src/containers/Routing/Routes.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import Login from '../Login';
+import AppliedRoute from '../AppliedRoute';
 import NotFound from './NotFound';
 
-const Routes = () => (
+const Routes = ({ appProps }) => (
 	<Switch>
-		<Route exact path="/" component={Home} />
-		<Route path='/login' exact component={Login}/>
+		{/* to use AppliedRoute component, we include it in the routes where we need to have the appProps passed in */}
+		<AppliedRoute exact path="/" component={Home} />
+		<AppliedRoute path='/login' exact component={Login} appProps={appProps} />
+		{ /* Finally, catch all unmatched routes */ }
 		<Route component={NotFound} />
 	</Switch>
 );
