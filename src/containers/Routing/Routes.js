eadd8c997093fc17ef20eddac2ac819713bb133a
agnenevulyte <you@example.com>
2020-01-01 17:05:37 +0000

    Add the Route to the Signup form

--- src/containers/Routing/Routes.js
+++ src/containers/Routing/Routes.js
@@ -4,12 +4,14 @@ import Home from '../Home';
 import Login from '../Login';
 import AppliedRoute from '../AppliedRoute';
 import NotFound from './NotFound';
+import Signup from '../Signup';
 
 const Routes = ({ appProps }) => (
 	<Switch>
 		{/* to use AppliedRoute component, we include it in the routes where we need to have the appProps passed in */}
 		<AppliedRoute exact path="/" component={Home} />
 		<AppliedRoute path='/login' exact component={Login} appProps={appProps} />
+		<AppliedRoute path='/signup' exact component={Signup} appProps={appProps} />
 		{ /* Finally, catch all unmatched routes */ }
 		<Route component={NotFound} />
 	</Switch>
