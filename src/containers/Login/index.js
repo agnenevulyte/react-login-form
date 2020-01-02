973b6b12f9aae5e78553b78c0193d09d891c163f
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-01-02 10:21:45 +0000

    added setTimeout for Login loader

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,76 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FormGroup, FormControl, Form } from "react-bootstrap";
-import LoaderButton from '../LoaderButton';
-import { Auth } from 'aws-amplify';
-import { useFormFields } from '../../libs/hooksLib'
+import LoaderButton from "../LoaderButton";
+import { Auth } from "aws-amplify";
+import { useFormFields } from "../../libs/hooksLib";
 import "./Login.css";
 
 export default function Login(props) {
-	const [isLoading, setIsLoading] = useState(false);
-	const [fields, handleFieldChange] = useFormFields({
-		email: "",
-		password: ""
-	});
-  
-	function validateForm() {
-	  return fields.email.length > 0 && fields.password.length > 0;
-	}
-  
-	function handleSubmit(event) {
-	  event.preventDefault();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
-	  setIsLoading(true);
-	  try {
-		  Auth.signIn(fields.email, fields.password);
-		  // alert('Logged in');
-		  props.userHasAuthenticated(true);
-		  // redirect to the home page once logged in
-		  props.history.push('/');
-		  console.log("-------------------logged in")
-	  } catch (e) {
-		  alert(e.message);
-		  console.log('-------------------some error message')
-		  setIsLoading(false);
-	  }
-	}
-  
-	return (
-	  <div className="Login">
-		<form onSubmit={handleSubmit}>
-		  <FormGroup controlId="email" bsSize="large">
-			<Form.Label>Email</Form.Label>
-			<FormControl
-			  autoFocus
-			  type="email"
-			  value={fields.email}
-			  onChange={handleFieldChange}
-			/>
-		  </FormGroup>
-		  <FormGroup controlId="password" bsSize="large">
-			<Form.Label>Password</Form.Label>
-			<FormControl
-			  value={fields.password}
-			  onChange={handleFieldChange}
-			  type="password"
-			/>
-		  </FormGroup>
-		  {/* <Button block bsSize="large" disabled={!validateForm()} type="submit">
+  function validateForm() {
+    return fields.email.length > 0 && fields.password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setIsLoading(true);
+    try {
+      Auth.signIn(fields.email, fields.password);
+      // alert('Logged in');
+      props.userHasAuthenticated(true);
+      // redirect to the home page once logged in
+      setTimeout(() => {
+        props.history.push("/");
+      }, 1000);
+      //   props.history.push("/");
+      console.log("-------------------logged in");
+    } catch (e) {
+      alert(e.message);
+      console.log("-------------------some error message");
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <Form.Label>Email</Form.Label>
+          <FormControl
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <Form.Label>Password</Form.Label>
+          <FormControl
+            value={fields.password}
+            onChange={handleFieldChange}
+            type="password"
+          />
+        </FormGroup>
+        {/* <Button block bsSize="large" disabled={!validateForm()} type="submit">
 			Login
 		  </Button> */}
-		  <LoaderButton
-			block
-			type="submit"
-			bsSize="large"
-			isLoading={isLoading}
-			disabled={!validateForm()}
-		  >
-			Login
-		  </LoaderButton>
-		</form>
-	  </div>
-	);
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Login
+        </LoaderButton>
+      </form>
+    </div>
+  );
 }
-
 
 // import React, { Component } from "react";
 //     import Form from 'react-bootstrap/Form'
