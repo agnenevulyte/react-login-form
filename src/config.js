d03d8eb4535d304ab1bb3bf2aab78991aea2ba1f
agnenevulyte <you@example.com>
2020-01-05 20:30:27 +0000

    AWS Amplify updated config.js

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 // https://serverless-stack.com/chapters/configure-aws-amplify.html
 export default {
-    s3: {
-      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
-    },
-    apiGateway: {
-      REGION: "YOUR_API_GATEWAY_REGION",
-      URL: "YOUR_API_GATEWAY_URL"
-    },
-    cognito: {
-      REGION: "YOUR_COGNITO_REGION",
-      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
-    }
-  };
+	s3: {
+		REGION: 'eu-west-1',
+		BUCKET: 'notes-app-2020-uploads'
+	},
+	apiGateway: {
+		REGION: 'eu-west-1',
+		URL: 'https://y12sq3p30k.execute-api.eu-west-1.amazonaws.com/prod'
+	},
+	cognito: {
+		REGION: 'eu-west-1',
+		USER_POOL_ID: 'eu-west-1_rtxoyILSU',
+		APP_CLIENT_ID: '2bta28m2d80od7e37od39kjhfi',
+		IDENTITY_POOL_ID: 'eu-west-1:5ead1410-cf12-478a-9895-1474f9386694'
+	}
+};
