2ad43ab8e808050d4aa72dfd6e23311fcd6c6a0e
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-01-06 11:07:35 +0000

    added DOB to the signup form

---
+++ src/containers/DOB/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+export default class DOB extends React.Component {
+  state = {
+    startDate: new Date()
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  render() {
+    return (
+      <DatePicker
+        className="form-control"
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+        peekNextMonth
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+      />
+    );
+  }
+}
